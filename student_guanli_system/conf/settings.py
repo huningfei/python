d78c007fe0bb5d87794cfe9c23c103770a6367a2
hu <huningfei@126.com>
2018-05-23 18:09:13 +0800

    学习笔记

---
+++ student_guanli_system/conf/settings.py
@@ -0,0 +1,14 @@
+#定义目录路径
+
+userinfo = '../db/userinfo'
+school_info = '../db/school_info'
+course_info = '../db/course_info'
+class_info = '../db/class_info'
+teacher_info = '../db/teacher_info'
+student_info = '../db/student_info'
+classes_dir = '../db/classes'
+
+# with open('../db/userinfo','r') as f1:
+#     print(f1.read())
+#     # for i in f1:
+#     #     print(i.strip())
